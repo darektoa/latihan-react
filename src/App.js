8a8e3a72cf75392da7986c793415c88978420ace
darektoa <darektoa@gmail.com>
2020-11-17 13:55:32 +0700

    First Push

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -26,4 +27,68 @@ function App() {
   );
 }
 
-export default App;
+
+
+const AppVar = ()=>{
+  const name = 'Abdul Fattah';
+  const grade = "XI - RPL"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{name}</p>
+        <p>{grade}</p>
+      </header>
+    </div>
+  );
+}
+
+
+
+class AppClass extends React.Component {
+  constructor(props){
+    super(props);
+    this.name = 'Abdul Fattah';
+    this.grade = "XI - RPL"
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>{this.name}</p>
+          <p>{this.grade}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export {
+  App,
+  AppVar,
+  AppClass
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App, AppVar, AppClass} from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppClass />
   </React.StrictMode>,
   document.getElementById('root')
 );
